acac7b0a3c5831bf7e315337237a9c9966d5b9c0
Christelle Nkera <scnkera@gmail.com>
2025-01-13 15:00:53 -0500

    Updated variable names

--- restaurants.py
+++ restaurants.py
@@ -1,7 +1,25 @@
 def most_varied_visitor(visits):
-    # Write your solution here!
-    pass
 
+    hash_table = {}
+
+    for visitors in visits.values():
+        unique_visitors = set(visitors)
+
+        for curr_visitor in unique_visitors:
+            if curr_visitor not in hash_table:
+                hash_table[curr_visitor] = 1
+            else:
+                hash_table[curr_visitor] += 1
+
+    best_visitor = None
+    best_count = None
+        
+    for visitor, count in hash_table.items():
+        if best_count is None or count > best_count:
+            best_visitor = visitor
+            best_count = count
+
+    return best_visitor
 
 visits_1 = {
     "Spicy City" : ["Eliza"],
